44d5784f2d2d186430f5824d3a85e1b00c7d62ea
jorandmol <jorandmol@alum.us.es>
2021-05-19 14:14:49 +0200

    Scrapping - Cambios
    He realizado cambios suficientes para obtener la información desde 2018 en adelante para las ligas LEC, LCS, LPL y LCK.

--- lolpedia/consultas/scrapping.py
+++ lolpedia/consultas/scrapping.py
@@ -1,45 +1,83 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import datetime
+import re
 
 # Funciones para hacer scrapping de la web
 
 URL_BASE = "https://lol.fandom.com"
-URL_WORLDS = "_World_Championship"
+INITIAL_YEAR = 2018
 NO_DATA = "Sin datos"
+REGIONS = ["LCS","LEC","LCK","LPL","NA LCS","EU LCS"]
+
+def get_team_clas(standings):
+    res = []
+    qualifyng = standings.find("table", class_="wikitable2 standings").find_all("tr", class_="teamhighlight")
+    for pos in qualifyng:
+        team_name = pos["data-teamhighlight"]
+        team_data = pos.find_all("td")
+        team_pos = team_data[0].get_text()
+        team_res = team_data[2].get_text()
+        team_vic = team_data[3].get_text()
+        res.append((team_pos,team_name,team_res,team_vic))
+    return res
 
 class LoL_pedia():
 
-    seasons = []
     competitions = []
+    splits = []
 
-    def get_seasons(self):
-        seasons = []
-        f = urllib.request.urlopen(URL_BASE+"/wiki/")
-        s = BeautifulSoup(f, "lxml")
-        content = s.find("div", id="global-wrapper").find("div", id="pageWrapper").find(id="bodyContent")
-        topnav = content.find("div", id="mw-content-text").find("div", class_="mw-parser-output").find("div", class_="mw-parser-output").find("div", class_="frontpage-top")
-        seasons_list = topnav.find_all("li")
-        for season in seasons_list:
-            season_url = URL_BASE+season.a["href"]+URL_WORLDS
-            season_year = season.a.get_text()
-            seasons.append((season_year,season_url))
-        self.seasons = seasons
+    def get_data(self):
+        self.get_competitions()
+        print("Se han almacenado " + len(self.competitions) + " competiciones en total")
+        self.get_splits()
+        print("con sus respectivos " + len(self.splits) + " splits")
 
-    def get_competitions_until17(self):
+    def get_competitions(self):
+        year_now = datetime.datetime.now().year
+        i = INITIAL_YEAR
         comp = []
-        regions = ["China","South Korea","Europe","North America"]
-        for season in self.seasons[1:7]:
-            f = urllib.request.urlopen(season[1])
+        while i < year_now+1:
+            f = urllib.request.urlopen(URL_BASE+"/wiki/"+str(i)+"_Season")
             s = BeautifulSoup(f, "lxml")
             content = s.find("div", id="global-wrapper").find("div", id="pageWrapper").find(id="bodyContent")
-            tabla = content.find("div", id="mw-content-text").find("div", class_="mw-parser-output").find("div", class_="mw-parser-output").find("table", class_="navbox").tbody.find("table")
-            trows = tabla.find_all("tr")
-            for tr in trows:
-                if tr.find("td"):
-                    region = tr.find("td").get_text()
-                    if region in regions:
-                        datos_comp = tr.find("td").next_sibling.find_all("li")
-                        for c in datos_comp:
-                            d = c.a
-                            comp.append((season[0],region,d.get_text(),d["href"]))
+            tabla = content.find("div", id="mw-content-text").find("div", class_="mw-parser-output").find("div", class_="mw-parser-output").find("table", class_="frontpagetable")
+            domestic_list = tabla.tbody.find_all("tr")[2].find("table", class_="navbox").find("table", class_="nowraplinks").tbody.find_all("tr")[4].find("div", class_="hlist").find_all("li")
+            for region in domestic_list:
+                reg_link = region.find("a")
+                if reg_link.get_text() in REGIONS:
+                    comp_url = URL_BASE+reg_link["href"]
+                    comp_name = reg_link.get_text()
+                    season_year = str(i)
+                    comp.append((season_year,comp_name,comp_url))
+            i += 1
         self.competitions = comp
+
+    def get_splits(self):
+        splits = []
+        for comp in self.competitions:
+            f = urllib.request.urlopen(comp[2])
+            s = BeautifulSoup(f, "lxml")
+            content = s.find("div", id="global-wrapper").find("div", id="pageWrapper").find(id="bodyContent")
+            data = content.find("div", id="mw-content-text").find("div", class_="mw-parser-output").find("div", class_="mw-parser-output").find_all("h2")[:2]
+            for split in data:
+                split_name = split.find("span").get_text()
+                clas = []
+                knock = []
+                standings = split.next_sibling.next_sibling
+                clasif = get_team_clas(standings)
+                clas = clasif
+                if comp[1] == "LPL" and comp[0] == "2018":
+                    standings = standings.next_sibling.next_sibling
+                    clasif = get_team_clas(standings)
+                    clas.extend(clasif)
+                knockout = standings.next_sibling.next_sibling
+                rounds = knockout.find_all("div", {"class": re.compile("^bracket-team round\d teamhighlight teamhighlighter")})
+                for team in rounds:
+                    if team.has_attr("data-teamhighlight"):
+                        r_team_name = team["data-teamhighlight"]
+                        points = team.find("div", class_="bracket-team-points").get_text()
+                        round = re.search("round\d", str(team["class"]))
+                        knock.append((round.group(0),r_team_name,points))
+                splits.append((comp[0],comp[1],split_name,clas,knock))
+        self.splits = splits
